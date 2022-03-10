8fb51ced544158f7fedd13bde53b316e283c27cd
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-10 19:31:43 +0300

    c task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,17 @@
+#include<stdio.h>
 
+int main(void)
+{
+	int intType;
+	float floatType;
+	long int longInt;
+	long long int longlongInt;
+	char charType;
 
+	printf("Size of a char: %i byte(s)\n", sizeof(charType));
+	printf("Size of an int: %i byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %i byte(s)\n", sizeof(longInt));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(longlongInt));
+	printf("Size of a float: %i byte(s)\n", sizeof(floatType));
+	return (0);
+}
