727fcae1c3bf25e400944bab4bf635ce646a2edc
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-11 23:54:10 +0300

    bonus

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of single-digit numbers,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a,b;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+    for (b = 0; b < 10; b++)
+    {
+      putchar(b + '0');
+		  if (a < 9)
+		  {
+			  putchar(',');
+			  putchar(' ');
+		  }
+    }
+	}
+	putchar('\n');
+
+	return (0);
+}
