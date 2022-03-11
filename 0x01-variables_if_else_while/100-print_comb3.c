dbbe3bd507ad1110a0bd3c43dafc6c76dba5729d
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-11 23:57:59 +0300

    bonus

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,17 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		putchar(a + '0');
-    for (b = 0; b < 10; b++)
-    {
-      putchar(b + '0');
-		  if (a < 9)
-		  {
-			  putchar(',');
-			  putchar(' ');
-		  }
-    }
+		
+	    for (b = 0; b < 10; b++)
+	    {
+		    putchar(a + '0');
+		    putchar(b + '0');
+			  if (a < 9)
+			  {
+				  putchar(',');
+				  putchar(' ');
+			  }
+	    }
 	}
 	putchar('\n');
 
