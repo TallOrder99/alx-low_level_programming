d273887203a4e56f985bf02f4ef89820595ae1c6
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-15 21:51:30 +0300

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
+#include "main.h"
+
 /**
- * main - check the code
+ *print_alphabet - printing lower case alphabets
+ *Description: print abc...z
  *
- * Return: Always 0.
+ *Return: Always 0 (Success)
  */
-void print_alphabet()
+void print_alphabet(void)
 {
-    for (char i='a';i<='z';i++)
-  {
-    putchar(i);
-  }
-}
-int main(void)
-{
-    print_alphabet();
-  
-    return (0);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		_putchar(alp[i]);
+	_putchar('\n');
 }
