ff0ef2913c2ebab2033d1ca58d51bbad1c19282d
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-15 22:38:05 +0300

    task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int x1 = 0, total = 0;
+	unsigned long int x2 = 1;
+	int i;
+	unsigned long  int sum = 0;
+
+	for (i = 2; i < 4000000; i++)
+	{
+		sum = x1 + x2;
+		x1 = x2;
+		x2 = sum;
+		if (sum % 2 == 0)
+		{
+			total += sum;
+		}
+
+	}
+	printf("%ld\n", total);
+	return (0);
+}
