e8e5f7b46939b10705a1fcbfa54ae73a5b1885ba
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-15 22:18:15 +0300

    task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_isalpha - checks if lawcase or not
+ * @c: accept char input
+ *Return: 1 if lowercase else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
