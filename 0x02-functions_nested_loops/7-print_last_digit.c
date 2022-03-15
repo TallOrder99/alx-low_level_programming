dbc4bcf06b769f9b344cab197ece5e3c151911a1
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-15 22:23:15 +0300

    task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *_abs - compute absolute value of a number
+ * @n: accept an integer
+ *Return: the absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
+/**
+ *print_last_digit - compute the last digit of the number
+ * @n: accept an integer
+ *Return: returns the last gigit of a number
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = _abs(n % 10);
+	_putchar(last + '0');
+	return (last);
+}
