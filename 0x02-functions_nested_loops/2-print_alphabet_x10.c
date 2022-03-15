1542c9b6f39fc2227fb4ddf7246e6d13be86f640
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-03-15 22:12:07 +0300

    task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - print lowercase alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
