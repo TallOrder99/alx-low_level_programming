fb4736d90dae2de97e31e89013467b6859944aa9
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-04-13 23:10:57 +0300

    commit task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,27 @@
 #include "3-calc.h"
-#include <string.h>
-
+#include <stdlib.h>
 /**
- * get_op_func - selects the correct operation to perform
- * @s: operation to perform
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
  *
- * Return: pointer to the correct function
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
-int (*get_op_func(char *s))(int, int) {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-while (ops[i].op != NULL)
-{
-if (strcmp(s, ops[i].op) == 0)
-{
-return (ops[i].f);
-}
-i++;
-}
-return (NULL);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
 }
