f34c3703df670cc56e0e09ff9f8020f58bd5b119
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-06-16 23:21:45 +0300

    main header

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,6 @@
-#ifndef M_H
-#define M_H
+#ifndef H_
+#define H_
 #include<stdio.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+#endi
