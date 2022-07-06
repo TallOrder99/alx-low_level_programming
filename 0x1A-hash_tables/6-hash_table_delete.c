4fa674aca505d723e3a0029d90d600d467d3853b
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-07-06 21:16:08 +0300

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,33 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - This function deletes a hash table.
- * @ht: The Hash table.
- *
- * Return: Nothing.
- **/
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *list, *tem;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
-
-	if (!ht)
-	{
-		return;
-	}
 
 	for (i = 0; i < ht->size; i++)
 	{
-		list = ht->array[i];
-		while (list)
+		if (ht->array[i] != NULL)
 		{
-			tem = list;
-			list = list->next;
-			free(tem->key);
-			free(tem->value);
-			free(tem);
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
